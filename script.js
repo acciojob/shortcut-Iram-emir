7e3c92361d35b572f26f08fdcc8537c6f70e6e82
iram-emir <158573737+Iram-emir@users.noreply.github.com>
2024-05-08 22:18:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function shortcut(str1, str2) {
     if (!str1 || !str2) {
         return '';
     } else {
-        return str1[0].toUpperCase() + str2[0].toLowerCase();
+        return str1[0] + str2[0];
     }
 }
 
@@ -11,8 +11,3 @@ console.log(shortcut("Amnesty", "International"));  // Output: 'AI'
 console.log(shortcut("Hello", "world"));            // Output: 'Hw'
 console.log(shortcut("", "International"));         // Output: ''
 console.log(shortcut("Amnesty", ""));               // Output: ''
-
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
